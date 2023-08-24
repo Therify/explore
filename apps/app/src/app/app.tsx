1751918e6c089a52f0161ac19d97f2576e34e9b6
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-24 09:56:41 -0500

    feat(app): correlate users from website

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -4,6 +4,14 @@ import { usePostHog } from 'posthog-js/react';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  const posthog = usePostHog();
+  useEffect(() => {
+    if (window.location.hash && window.location.hash.includes('id')) {
+      const [, id] = window.location.hash.split('=');
+      posthog.identify(id);
+    }
+  }, [posthog]);
+
   return (
     <div>
       <NxWelcome title="app" />
