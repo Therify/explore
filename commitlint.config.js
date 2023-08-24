2641861af1e24a340e115b265e4479e7c37d837b
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-24 06:44:36 -0500

    chore: extend commitlint for nx

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const {
+  utils: { getProjects },
+} = require("@commitlint/config-nx-scopes");
+
+module.exports = {
+  extends: ["@commitlint/config-conventional", "@commitlint/config-nx-scopes"],
+  rules: {
+    "scope-enum": async (ctx) => [
+      2,
+      "always",
+      [
+        ...(await getProjects(
+          ctx,
+          ({ name, projectType }) =>
+            !name.includes("e2e") && projectType == "application"
+        )),
+      ],
+    ],
+  },
+};
