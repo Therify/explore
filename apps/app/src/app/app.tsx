718d75a226a1c31e2ce3ff99bb49159f8948eddd
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-24 08:13:49 -0500

    chore(app): track with posthog

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { usePostHog } from 'posthog-js/react';
+
 import NxWelcome from './nx-welcome';
 
 export function App() {
--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 import App from './app/app';
+
+posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>
 );
