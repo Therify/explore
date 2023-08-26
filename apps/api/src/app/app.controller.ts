7ca64a846dec163e3da6fe6e17228d1ecc3c95df
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-26 12:04:27 -0500

    chore: with prisma

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService
+  ) {}
+  @Get('plus-one/:num')
+  async plusOne(@Param('num') num: number): Promise<void> {
+    const res = await this.prisma.$queryRaw`select 1 + ${num}::int as result`;
+    console.info(res);
+  }
   @Get()
   getData() {
     return this.appService.getData();
