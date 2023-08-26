1ea15aee9cc3c9698bc039b10272ca6d71d5352c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-08-26 10:46:25 -0500

    feat(app): add router

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,3 +1,11 @@
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  Router,
+  Route,
+  RootRoute,
+} from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
@@ -8,10 +16,55 @@ import { PostHogProvider } from 'posthog-js/react';
 
 import App from './app/app';
 
+const rootRoute = new RootRoute({
+  component: Root,
+});
+
+function Root() {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  );
+}
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: App,
+});
+
+const aboutRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/about',
+  component: About,
+});
+
+function About() {
+  return <div>Hello from About!</div>;
+}
+
+// Create the route tree using your routes
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+// Create the router using your route tree
+const router = new Router({ routeTree });
+
+// Register your router for maximum type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
 });
-console.debug('API Endpoint', import.meta.env.VITE_API_ENDPOINT);
+
 Sentry.init({
   dsn: 'https://432d48e455fc6e9b67f1318e4a51dc8d@o4505760600883200.ingest.sentry.io/4505760604291072',
   integrations: [
@@ -39,7 +92,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <PostHogProvider client={posthog}>
-      <App />
+      <RouterProvider router={router} />
     </PostHogProvider>
   </StrictMode>
 );
